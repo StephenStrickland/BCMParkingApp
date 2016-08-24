18fae39de4333eb4785d2c446def16addea6eba0
Stephen Strickland <stephen@startup-rescue.com>
2016-08-24 13:19:56 -0400

    contains a working jellybean app. includes activities for users, members, and plates.

--- Android/BcmParking/app/src/main/java/com/ksubcm/parking/bcmparking/LoginActivity.java
+++ Android/BcmParking/app/src/main/java/com/ksubcm/parking/bcmparking/LoginActivity.java
@@ -3,6 +3,7 @@ package com.ksubcm.parking.bcmparking;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,10 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.ksubcm.parking.bcmparking.activities.LicensePlatesListActivity;
+import com.ksubcm.parking.bcmparking.activities.MembersActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -81,16 +86,23 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
+//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+//        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    public void loginBtnClick(View view){
+        Intent intent = new Intent(this, LicensePlatesListActivity.class);
+        startActivity(intent);
+        //attemptLogin();
+
     }
 
     private void populateAutoComplete() {
@@ -184,8 +196,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+
+
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
         }
     }
     private boolean isEmailValid(String email) {
@@ -331,6 +345,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
---
+++ Android/BcmParking/app/src/main/java/com/ksubcm/parking/bcmparking/activities/MembersActivity.java
@@ -0,0 +1,18 @@
+package com.ksubcm.parking.bcmparking.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ksubcm.parking.bcmparking.R;
+
+/**
+ * Created by Stephen on 8/23/16.
+ */
+
+public class MembersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_members);
+    }
+}
